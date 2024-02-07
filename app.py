9e10a5878a7e3c07acc3d7b817b2144e4ea72e46
Fouad Mhz <73448082+Fouad-Mhz@users.noreply.github.com>
2024-02-07 02:54:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
-from pages import inference_page, base_models_page, meta_model_page
+from pages import inference_page
 
 def main():
     st.sidebar.title("Navigation")
 
     # Create a sidebar with page options
-    page_options = ["Inference", "Meta model monitoring", "Base models monitoring" ]
+    page_options = ["Inference", ]
     selected_page = st.sidebar.radio("Select Page", page_options)
 
     # Render the selected page
     if selected_page == "Inference":
         inference_page.show()
-    elif selected_page == "Meta model monitoring":
-        meta_model_page.show()
-    elif selected_page == "Base models monitoring":
-        base_models_page.show()
 
 if __name__ == "__main__":
     main()
